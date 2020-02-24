3f2d20ff0792d9dcd9985fd3b991f3697fee5aa5
Warfor <44639824+Warfor@users.noreply.github.com>
2020-02-24 13:48:17 +0100

    yeyeye

--- Mobilappers/app/src/main/java/com/example/mobilappers/MainActivity.java
+++ Mobilappers/app/src/main/java/com/example/mobilappers/MainActivity.java
@@ -13,30 +13,36 @@ public class MainActivity extends AppCompatActivity {
     public static final Random RANDOM = new Random();
     private Button rollDices;
     private ImageView imageView1, imageView2;
+    private int[] pics;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        rollDices =(Button)findViewById(R.id.rollDices);
-        imageView1 =(ImageView) findViewById(R.id.imageView1);
-        imageView2 =(ImageView) findViewById(R.id.imageView2);
+        pics = new int[]{R.drawable.dice1, R.drawable.dice2, R.drawable.dice3, R.drawable.dice4, R.drawable.dice5, R.drawable.dice6};
+            rollDices = findViewById(R.id.rollDices);
+            imageView1 = findViewById(R.id.imageView1);
+            imageView2 = findViewById(R.id.imageView2);
         rollDices.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                int value1 = randomDiceValue();
                int value2 = randomDiceValue();
 
-               int res1 = getResources().getIdentifier("dice_" + value1, "drawable", "com.example-mobilappers");
-               int res2 = getResources().getIdentifier("dice_" + value2, "drawable", "com.example-mobilappers");
-
-               imageView1.setImageResource(res1);
-               imageView2.setImageResource(res2);
+               //int res1 = getResources().getIdentifier("dice1", "drawable", "com.example-mobilappers");
+               //int res2 = getResources().getIdentifier("dice1", "drawable", "com.example-mobilappers");
+               imageView1.setImageResource(pics[value1]);
+               imageView2.setImageResource(pics[value2]);
 
             }
         });
     }
     public static int randomDiceValue() {
-        return RANDOM.nextInt(6)+1;
+        return RANDOM.nextInt(6);
+    }
+
+    public void readRolls()
+    {
+
     }
 
 
