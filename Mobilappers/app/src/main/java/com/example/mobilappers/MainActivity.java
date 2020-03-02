f71461d5d8252d8b7f252749ac06f15bc5bb4eda
Wezzydw <44640169+Wezzydw@users.noreply.github.com>
2020-03-02 14:55:57 +0100

    done

--- Mobilappers/app/src/main/java/com/example/mobilappers/MainActivity.java
+++ Mobilappers/app/src/main/java/com/example/mobilappers/MainActivity.java
@@ -5,8 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Spinner;
 
 import java.util.ArrayList;
 import java.util.Random;
@@ -14,67 +16,113 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
     public static final Random RANDOM = new Random();
     private Button rollDices;
-    private ImageView imageView1, imageView2;
+    private ImageView imageView1, imageView2,imageView3,imageView4,imageView5,imageView6;
     private int[] pics;
     private Button nextView;
-    private ArrayList<String> rolls;
-    int SecondActivity = 10;
+    private ArrayList<String> rolls = new ArrayList<>();
+    private Spinner spinner;
+    Bundle data;
+    private ArrayList<ImageView> imageViews = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        rolls = new ArrayList<>();
-        pics = new int[]{R.drawable.dice1, R.drawable.dice2, R.drawable.dice3, R.drawable.dice4, R.drawable.dice5, R.drawable.dice6};
-            rollDices = findViewById(R.id.rollDices);
-            imageView1 = findViewById(R.id.imageView1);
-            imageView2 = findViewById(R.id.imageView2);
-            nextView = findViewById(R.id.btnNextView);
 
-            nextView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-            nextView();
-                }
-            });
+        if(getIntent() != null)
+        {
+            data= getIntent().getExtras();
+            if( data != null &&data.getBoolean("cleared"))
+            {
+                rolls.clear();
+            }
+        }
+
+        if(savedInstanceState != null)
+        {
+            rolls = savedInstanceState.getStringArrayList("array");
+
+        }
+        else
+        {
+            rolls  = new ArrayList<>();
+
+        }
+
+        pics = new int[]{R.drawable.dice1, R.drawable.dice2, R.drawable.dice3, R.drawable.dice4, R.drawable.dice5, R.drawable.dice6};
+        rollDices = findViewById(R.id.rollDices);
+        imageView1 = findViewById(R.id.imageView1);
+        imageView2 = findViewById(R.id.imageView2);
+        imageView3 = findViewById(R.id.imageView3);
+        imageView4 = findViewById(R.id.imageView4);
+        imageView5 = findViewById(R.id.imageView5);
+        imageView6 = findViewById(R.id.imageView6);
+        nextView = findViewById(R.id.btnNextView);
+        spinner = findViewById(R.id.spinner);
+
+        imageViews.add(imageView1);
+        imageViews.add(imageView2);
+        imageViews.add(imageView3);
+        imageViews.add(imageView4);
+        imageViews.add(imageView5);
+        imageViews.add(imageView6);
+
+        nextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nextView();
+            }
+        });
+
 
         rollDices.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               int value1 = randomDiceValue();
-               int value2 = randomDiceValue();
 
-               //int res1 = getResources().getIdentifier("dice1", "drawable", "com.example-mobilappers");
-               //int res2 = getResources().getIdentifier("dice1", "drawable", "com.example-mobilappers");
+                int numberOfDice = Integer.parseInt(spinner.getSelectedItem().toString());
+                String currentRolls = "";
 
+                for (int i = 0; i< numberOfDice; i++)
+                {
+                    int value = randomDiceValue();
+                    imageViews.get(i).setImageResource(pics[value]);
+                    currentRolls += (value+1) + ":";
+                }
 
-               imageView1.setImageResource(pics[value1]);
-               imageView2.setImageResource(pics[value2]);
-               rolls.add("" + (value1+1) + ":" + (value2+1));
+                if(numberOfDice < 6)
+                {
+                    for (int i = numberOfDice; i <6; i++)
+                    {
+                        imageViews.get(i).setImageResource(0);
+                    }
+                }
+
+                rolls.add(currentRolls);
 
             }
         });
     }
+
     public static int randomDiceValue() {
         return RANDOM.nextInt(6);
     }
 
-    public void readRolls()
-    {
+
+    private void nextView() {
+
+        Intent intent = new Intent(this, SecondActivity.class);
+
+        intent.putStringArrayListExtra("Rolls", rolls);
+        startActivity(intent);
 
     }
 
-    private void nextView()
+
+    @Override
+    protected void onSaveInstanceState(Bundle state)
     {
-
-        Intent intent = new Intent(this, SecondActivity.class);
-        System.out.println("test");
-        intent.putStringArrayListExtra("Rolls",  rolls);
-        startActivity(intent);
-
-
-    //View v = findViewById(R.id.listofrolls);
-       //setContentView(R.layout.listofrolls);
+        super.onSaveInstanceState(state);
+        state.putStringArrayList("array", rolls);
     }
 
 
--- Mobilappers/app/src/main/java/com/example/mobilappers/SecondActivity.java
+++ Mobilappers/app/src/main/java/com/example/mobilappers/SecondActivity.java
@@ -2,7 +2,9 @@ package com.example.mobilappers;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
@@ -11,18 +13,37 @@ import androidx.appcompat.app.AppCompatActivity;
 import java.util.ArrayList;
 
 public class SecondActivity extends AppCompatActivity {
-int SecondActivity = 10;
+
     ListView list;
-    ArrayList arrayList;
+
+    Button back;
+    Button clear;
+    ArrayList<String> test = new ArrayList<>();
+    Boolean hasBeenCleared = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        System.out.println("test");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.listofrolls);
         list = findViewById(R.id.ListView);
-        //Intent intent = new Intent(this, MainActivity.class);
+        back = findViewById(R.id.btnBack);
+        clear = findViewById(R.id.btnClear);
 
-        //startActivityForResult(intent, 10);
+
+
+        clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clearList();
+            }
+        });
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Back();
+            }
+        });
 
         Bundle data = getIntent().getExtras();
 
@@ -30,6 +51,21 @@ int SecondActivity = 10;
 
         ListAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, rolls);
         list.setAdapter(adapter);
+
+    }
+
+    private void clearList()
+    {
+        ListAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, test);
+        list.setAdapter(adapter);
+        hasBeenCleared = true;
+    }
+
+    private void Back()
+    {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("cleared", hasBeenCleared);
+        startActivity(intent);
     }
 
 }
